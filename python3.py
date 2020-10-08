9267cc8a50502cd2916d0f9644c2a40d81702001
Haein.Kim <68260003+haein-kim@users.noreply.github.com>
2020-10-08 21:36:59 +0900

    Update python3.py

--- python3.py
+++ python3.py
@@ -1,4 +1,4 @@
---https://programmers.co.kr/learn/courses/30/lessons/12903
+-- 문제 https://programmers.co.kr/learn/courses/30/lessons/12903
 def solution(s):
     if len(s) %2 == 0 :
         return s[len(s)//2 -1 : len(s)//2 +1]   	
